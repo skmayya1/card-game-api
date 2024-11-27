c64377b57f7b6feed0fe62d217298fe8a4a7ff0e
skmayya1 <skandamayya4@gmail.com>
2024-11-27 22:51:58 +0530

    join room

--- src/Socket.ts
+++ src/Socket.ts
@@ -2,7 +2,9 @@ import io from ".";
 
 io.on('connection', (socket) => {
     console.log('a user connected ', socket.id);
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,22 @@ httpserver.listen(port, () => {
 export default io;
 
 io.on('connection', (socket) => {
-    console.log('a user connected ', socket.id);
+    console.log('A user connected', socket.id);
+
+    // Join Room
+    socket.on('join-room', (data) => {
+        console.log('join-room', data);
+        socket.join(data.roomID); // Use roomID for consistency
+
+        // Emit that the user connected
+        socket.to(data.roomID).emit('user-connected');
+
+        // Get users in the room
+        const usersInRoom = Array.from(io.sockets.adapter.rooms.get(data.roomID) || []);
+        io.to(data.roomID).emit('room-users', { users: usersInRoom });
+    });
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected');
     });
 });
